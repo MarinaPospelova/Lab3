836472ae02d4be389a4d6fc6d3c5aa2f838cc52c
MarinaPospelova <72177251+MarinaPospelova@users.noreply.github.com>
2021-03-10 10:21:56 +0700

    Add files via upload

---
+++ FlightH3 (1)/Flight/Flight.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+    class Flight
+    {
+        public double a;
+        public double v0;
+        public double y0;
+        public double S;
+        public double m;
+        public double k;
+
+        public double t;
+        public double x;
+        public double y;
+        public double vx;
+        public double vy;
+
+        const double dt = 0.01;
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        public void TakeValues(decimal Angle, decimal Speed, decimal Height, decimal Weight, decimal Square)
+        {
+            a = (double)Angle;
+            v0 = (double)Speed;
+            y0 = (double)Height;
+            m = (double)Weight;
+            S = (double)Square;
+        }
+
+        public void StartValues()
+        {
+            vx = v0 * Math.Cos(a * Math.PI / 180);
+            vy = v0 * Math.Sin(a * Math.PI / 180);
+            t = 0;
+            x = 0;
+            y = y0;
+        }
+
+        public void Calculation(double S, double m)
+        {
+            k = 0.5 * C * S * rho / m;
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+        }
+    }
+}
---
+++ FlightH3 (1)/Flight/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        Flight Flight = new Flight();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Flight.TakeValues(edAngle.Value, edSpeed.Value, edHeight.Value, edWeight.Value, edSquare.Value);
+            Flight.StartValues();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(Flight.x, Flight.y);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Flight.Calculation(Flight.S, Flight.m);
+
+            chart1.Series[0].Points.AddXY(Flight.x, Flight.y);
+            if (Flight.y <= 0) timer1.Stop();
+        }
+    }
+
+}
